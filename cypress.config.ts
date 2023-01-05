de4e70ab6244cb11ed1559647df8f803359af3c4
Aszton <bartoszkuczera88@gmail.com>
2023-01-05 22:53:06 +0100

    fixtures topic

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  e2e: {
+    experimentalModifyObstructiveThirdPartyCode: true,
+    viewportWidth: 1440,
+    viewportHeight: 900,
+    defaultCommandTimeout: 7000,
+    pageLoadTimeout: 15000,
+    requestTimeout: 5000,
+    setupNodeEvents(on, config) {},
+  },
+});
