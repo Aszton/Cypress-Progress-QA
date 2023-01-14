b70ffa98e1f1f9ec97f80bf83c44023332528530
Aszton <bartoszkuczera88@gmail.com>
2023-01-14 22:39:50 +0100

    added Day JS test

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   chromeWebSecurity: false,
   e2e: {
+    chromeWebSecurity: false,
     experimentalModifyObstructiveThirdPartyCode: true,
     viewportWidth: 1440,
     viewportHeight: 900,
